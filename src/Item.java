90be5c20ce9e982e79d42bb0bc265ceaf10cda9a
xtekkis <211207676+xtekkis@users.noreply.github.com>
2026-05-21 16:48:43 +0300

    Added item class with name, description, heal amount and value

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,29 @@
 public class Item {
+    private String name;
+    private String description;
+    private int healAmount;
+    private int value;
+
+    // Constructor
+    public Item(String name, String description, int healAmount, int value) {
+        this.name = name;
+        this.description = description;
+        this.healAmount = healAmount;
+        this.value = value;
+    }
+
+    // Getters
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public int getHealAmount() { return healAmount; }
+    public int getValue() { return value; }
+
+    // Display item info
+    public void displayInfo() {
+        System.out.println(name + " - " + description);
+        if (healAmount > 0) {
+            System.out.println("Heals: " + healAmount + " HP");
+        }
+        System.out.println("Value: " + value + " gold");
+    }
 }
