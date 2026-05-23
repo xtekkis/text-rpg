9c5a6c200f06e5a22be553f47767f789d738a738
xtekkis <211207676+xtekkis@users.noreply.github.com>
2026-05-23 19:57:17 +0300

    Added buildWorld method with rooms, enemies and items

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,37 @@ public class Main {
 
         // Create player
         player = new Player(name);
+
+        // Build the world
+        buildWorld();
+
         System.out.println("\nWelcome, " + player.getName() + "! Your adventure begins...");
+    }
+
+    private static void buildWorld() {
+        // Create rooms
+        Room entrance = new Room("Entrance", "You stand at the entrance of a dark dungeon.");
+        Room hallway = new Room("Hallway", "A long dark hallway stretches before you.");
+        Room treasury = new Room("Treasury", "A dusty room filled with old chests.");
+        Room bossRoom = new Room("Boss Chamber", "A massive chamber. Something evil lurks here.");
+
+        // Connect rooms
+        entrance.setNorth(hallway);
+        hallway.setSouth(entrance);
+        hallway.setNorth(bossRoom);
+        hallway.setEast(treasury);
+        treasury.setWest(hallway);
+        bossRoom.setSouth(hallway);
+
+        // Create and place enemies
+        hallway.setEnemy(new Enemy("Goblin", 30, 10, 15));
+        bossRoom.setEnemy(new Enemy("Dark Knight", 80, 25, 50));
+
+        // Create and place items
+        entrance.setItem(new Item("Health Potion", "Restores 30 HP", 30, 10));
+        treasury.setItem(new Item("Elixir", "Restores 50 HP", 50, 25));
+
+        // Start in entrance
+        currentRoom = entrance;
     }
 }
